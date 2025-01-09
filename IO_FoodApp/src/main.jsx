b96794368d83cb25b4508efa47a22fc5512c27f6
Iza <szpak644@gmail.com>
2025-01-09 19:53:51 +0100

    Fixing qr code generator

--- IO_FoodApp/src/main.jsx
+++ IO_FoodApp/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './header.jsx'
-//import QR from './QR.jsx'
+import QR from './QR.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Header />
         <App />
+        <QR />
   </StrictMode>,
 )
